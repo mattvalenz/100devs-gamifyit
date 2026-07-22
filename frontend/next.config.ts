2c7e5b1df2d353e8315822414bfdceaaea5bb1a0
Obinna Ezeka <obinnae815@gmail.com>
2026-07-21 17:23:04 -0700

    revert next.config.ts to original state

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 module.exports = {
   allowedDevOrigins: ['127.0.0.1'],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com'
-      }
-    ]
-  }
 }
 
 const nextConfig: NextConfig = {
